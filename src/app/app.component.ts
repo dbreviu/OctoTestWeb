dd81fce6e811d6dcefd4fb575812dec12b6f3209
Dan Breviu <daniel.breviu@polaris.com>
2016-09-30 13:17:48 -0500

    animations!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, trigger, state, style, transition, animate, keyframes } from '@angular/core';
 
 import { Person } from './Person';
 import './rxjs-operators';
@@ -10,6 +10,29 @@ import { Subject }          from 'rxjs/Subject';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+animations: [
+  trigger('flyInOut', [
+    state('in', style({transform: 'scale(0)'})),
+    transition('void => *', [
+      style({transform: 'scale(0)'}),
+      animate(300, keyframes([
+        style({opacity: 0, transform: 'scale(0)', offset: 0}),
+        style({opacity: 1, transform: 'scale(1.3)',  offset: 0.7}),
+        style({opacity: 1, transform: 'scale(1)',     offset: 1.0})
+      ]))
+    ]),
+    transition('* => void', [
+      animate(300, keyframes([
+        style({opacity: 1, transform: 'translateY(100%)',     offset: 0}),
+        style({opacity: .5, transform: 'translateY(150px)', offset: 0.4}),
+        style({opacity: .2, transform: 'translateY(300px)',  offset: 1.0})
+       /* style({opacity: 1, transform: 'scale(1)', offset: 0}),
+        style({opacity: .5, transform: 'scale(.5)',  offset: 0.3}),
+        style({opacity: 0, transform: 'scale(0)',     offset: 1.0})*/
+      ]))
+    ])
+  ])
+],
   providers:[PersonService]
 })
 export class AppComponent {
